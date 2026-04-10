3734d7bcc54b7228e6eb20148bd9cca8579d9cba
Ben Gilden <268783001+bengilden-lgtm@users.noreply.github.com>
2026-04-10 21:17:05 +1200

    Remove the audit_tickets function from the memory module, which previously identified "angry" users and suggested empathetic responses. This deletion streamlines the codebase by eliminating unused functionality.

--- def audit_tickets(memory).py
+++
@@ -1,7 +0,0 @@
-def audit_tickets(memory):
-    angry = [m for m in memory if "angry" in m["user"].lower()]
-
-    if not angry:
-        return None
-
-    return "New Rule: Be more empathetic in early responses."
